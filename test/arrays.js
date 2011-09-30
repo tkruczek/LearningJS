e09a2c526e00e4825b1c2c21b13e41a8aaf6dbbc
Tomasz Kruczek <t.kruczek.pl@gmail.com>
2011-09-30 12:02:06 +0200

    2 commit

---
+++ test/arrays.js
@@ -0,0 +1,9 @@
+TestCase('ArrayTest', {
+	'test array splice should modify array' : function() {
+		var arr = [1, 2, 3, 4, 5];
+		var result = arr.splice(2, 3);
+		assertEquals([1, 2], arr);
+	}
+});
+
+
